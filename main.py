2352afd38dbbd706df597ba60ce8897dd0cac9fc
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2026-07-03 11:30:18 +0200

    push test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ s3 = boto3.client("s3")
 
 def lambda_handler(event, context):
     print("### Lambda started ###")
-    print("### Lambda started Lambda and AWS is great! ###")
+    print("### Lambda started Lambda and AWS is great! TEST 2 ###")
 
     try:
         # ---------------------------
