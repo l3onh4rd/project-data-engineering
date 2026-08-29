b33c9a929e1d1342c7406dd2735cd74b531fa8ab
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2026-08-29 14:09:12 +0200

    consuming parquet file

--- report.py
+++ report.py
@@ -7,31 +7,31 @@ s3 = boto3.client("s3")
 
 
 def lambda_handler(event, context):
-    print('42 Hello Report 42')
-    print('42 Hello Report 42')
+    # print('42 Hello Report 42')
 
     try:
         # Step 1 - Parse Event
-        # record = event["Records"][0]
+        record = event["Records"][0]
 
-        # bucket = record["s3"]["bucket"]["name"]
-        # key = urllib.parse.unquote_plus(record["s3"]["object"]["key"])
+        bucket = record["s3"]["bucket"]["name"]
+        key = urllib.parse.unquote_plus(record["s3"]["object"]["key"])
 
-        # # Step 2 - Parse paths
-        # filename = key.split("/")[-1]
+        # Step 2 - Parse paths
+        filename = key.split("/")[-1]
 
-        # download_path = f"/tmp/{filename}"
+        download_path = f"/tmp/{filename}"
         # output_path = "/tmp/output.parquet"
 
         # # Step 3 - Download File
-        # s3.download_file(bucket, key, download_path)
+        s3.download_file(bucket, key, download_path)
 
-        # # Step 4 - Read CSV
-        # print("Reading CSV...")
-        # df = pd.read_csv(download_path)
-        # # Printing Debugging Information
-        # print("DF SHAPE:", df.shape)
-        # print("DF COLUMNS:", df.columns.tolist())
+        # # Step 4 - Read Parquet
+        print("Reading CSV...")
+        df = pd.read_parquet(download_path)
+        # Printing Debugging Information
+        print("DF SHAPE:", df.shape)
+        print("DF COLUMNS:", df.columns.tolist())
+        print("DF COLUMNS:", df.size)
 
         # Step 7 - Upload
         # s3.upload_file(
